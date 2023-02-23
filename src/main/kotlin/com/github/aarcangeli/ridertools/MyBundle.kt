37e6f12406ee6950a09ed5fbbd8fd08ba9bca44e
GitHub Action <action@github.com>
2023-02-23 20:30:31 +0000

    Template cleanup

--- src/main/kotlin/com/github/aarcangeli/ridertools/MyBundle.kt
+++ src/main/kotlin/com/github/aarcangeli/ridertools/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.aarcangeli.ridertools
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/aarcangeli/ridertools/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/aarcangeli/ridertools/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.aarcangeli.ridertools.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.aarcangeli.ridertools.MyBundle
 
 class MyApplicationService {
 
